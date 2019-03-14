c6f4bff7b97032b516bf8fa535a095b556053e11
Max Fischer <maxfischer2781@gmail.com>
2019-03-14 11:27:28 +0100

    updated example configs to use currently recommended style

--- demo_config.py
+++ demo_config.py
@@ -3,6 +3,4 @@ from cobald.controller.linear import LinearController
 from cobald_demo.cpu_pool import CpuPool
 from cobald_demo.draw_line import DrawLineHook
 
-pool = CpuPool()
-hook = DrawLineHook(pool)
-controller = LinearController(hook, low_utilisation=0.9, high_allocation=1.1)
+pipeline = LinearController.s(low_utilisation=0.9, high_allocation=1.1) >> DrawLineHook.s() >> CpuPool(interval=1)
